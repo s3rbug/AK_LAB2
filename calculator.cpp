c384d39c7b9d8836d328c25d600e0cf43cd9cdd5
s3rbug <bugaychuk.sergiy@gmail.com>
2022-09-20 20:03:53 +0300

    add comments and division operator

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
+// a + b
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+// a - b
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// a * b
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+// a / b, if b equals zero return zero
+int Calculator::Div (double a, double b)
+{
+    return b == 0 ? 0 : a / b + 0.5;
 }
